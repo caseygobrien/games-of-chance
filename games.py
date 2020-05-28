7e9da7ebb76d1ed2d5a48dbc0122ab4772c0e278
Star Blazer <casey.g.obrien@gmail.com>
2020-05-27 22:42:32 -0700

    fully playable

--- games.py
+++ games.py
@@ -3,28 +3,25 @@ money = 1000
 # Write your game of chance functions here
 
 
-def coin_flip(bet=5, call="heads"):
+def coin_flip(bet, call):
 	global money
-	if call.lower() != "heads" and call.lower() != "tails":
-		return
+	money -= bet
 	flip = random.randint(1, 2)
 	if flip == 1:
 		flip = "heads"
-		print("The coin has landed on heads.")
+		print("The coin landed on heads.")
 	else:
 		flip = "tails"
-		print("The coin has landed on tails")
+		print("The coin landed on tails")
 	if flip == call:
-		print("You have won ${}".format(bet))
-		money += bet
+		print("You won ${}".format(bet))
+		money += bet * 2
 	else:
-		print("You have lost ${}".format(bet))
-		money -= bet
+		print("You lost ${}".format(bet))
 
-def cho_han(bet=5, call="even"):
+def cho_han(bet, call):
 	global money
-	if call.lower() != "odd" and call.lower() != "even":
-		return
+	money -= bet
 	roll = 0
 	for i in range(2):
 		die = random.randint(1, 6)
@@ -36,14 +33,13 @@ def cho_han(bet=5, call="even"):
 		roll = "odd"
 	if call == roll:
 		print("You have won ${}".format(bet))
-		money += bet
+		money += bet * 2
 	else:
 		print("You have lost ${}".format(bet))
-		money -= bet
 
-
-def highest_card(bet=5):
+def highest_card(bet):
 	global money
+	money -= bet
 	player1_card = ''
 	player2_card = ''
 	deck_of_cards = {"Spades": [], "Hearts": [], "Clubs": [], "Diamonds": []}
@@ -71,23 +67,17 @@ def highest_card(bet=5):
 	print("You drew the {}, and your opponent drew the {}".format(player1_card, player2_card))
 	if player1_num == player2_num:
 		print("It's a tie")
-		print("You have ${} remaining".format(money))
 	elif player1_num > player2_num:
-		money += bet
+		money += bet * 2
 		print("You have won ${}".format(bet))
-		print("You have ${} remaining".format(money))
 	else:
-		money -= bet
 		print("You have lost ${}".format(bet))
-		print("You have ${} remaining".format(money))
 
 	
 def roulette(betting):
 	# possible bets in roulette are individual numbers, red, black, odd, and even
-	# kwargs will be {what you're betting on : bet amount}
+	# arguments will be {what you're betting on : bet amount}
 	global money
-	for bets in betting.values():
-		money -= bets
 	wheel = []
 	black_numbers = []
 	for number in range(-1, 37):
@@ -115,7 +105,7 @@ def roulette(betting):
 	print("Your Bets")
 	for key, value in betting.items():
 		print("{}: ${}".format(key, value))
-	print("=" * 30)
+	print("-" * 30)
 	for key, value in betting.items():
 		if key == '00':
 			betting[-1] = betting.pop("00")
@@ -172,53 +162,219 @@ def roulette(betting):
 
 
 # Call your game of chance functions here
+print()
+print("Welcome to our exciting text-based casino!")
 playing = True
 while playing:
 	if money == 0:
 		print("You are out of money")
+		print("Better luck next time!")
 		break
 	print("You have ${}".format(money))
+	print()
 	game = input("""Choose a game:
-	[F]lip coin
-	[C]ho Han
-	[H]igh card
-	[R]oulette
-	[Q]uit
-	""").lower()
+[F]lip coin
+[C]ho Han
+[H]igh card
+[R]oulette
+[Q]uit
+""").lower()
 	if game == 'f':
-		coin = input("Heads or tails?\n").lower()
-		if coin != "heads" and coin != "tails":
-			print("Please choose 'heads' or 'tails'")
+		flipping = True
+		while flipping:
+			coin = input("Heads or tails?\n").lower()
+			if coin != "heads" and coin != "tails":
+				print("Please choose 'heads' or 'tails'")
+				print()
+				continue
+			try:
+				bet = int(input("Your bet?\n"))
+			except ValueError:
+				print("Bet must be an interger")
+				print()
+				continue
+			if bet > money:
+				print("You don't have enough money to make that bet")
+				print()
+				continue
 			print('-' * 30)
+			coin_flip(bet, coin)
+			print('-' * 30)
+			print("You have ${} remaining".format(money))
+			if money ==0:
+				flipping = False
+				continue
+			again = input("Play again? (y/n)\n").lower()
 			print()
-			continue
-		bet = int(input("Your bet?\n"))
-		if bet > money:
-			print("You don't have enough money to make that bet")
-			continue
-		coin_flip(bet, coin)
-		print('-' * 30)
-		print()
+			if again == 'y':
+				continue
+			else:
+				flipping = False
 	elif game == "c":
-		dice = input("Odd or even?\n").lower()
-		if dice != "odd" and dice != "even":
-			print("Please choose 'odd' or 'even'")
+		rolling = True
+		while rolling:
+			dice = input("Odd or even?\n").lower()
+			if dice != "odd" and dice != "even":
+				print("Please choose 'odd' or 'even'")
+				print()
+				continue
+			try:
+				bet = int(input("Your bet?\n"))
+			except ValueError:
+				print("Bet must be an integer")
+				print()
+				continue
+			if bet > money:
+				print("You don't have enough money to make that bet")
+				print()
+				continue
 			print('-' * 30)
+			cho_han(bet, dice)
+			print('-' * 30)
+			print("You have ${} remaining".format(money))
+			if money == 0:
+				rolling = False
+				continue
+			again = input("Roll again? (y/n)\n").lower()
 			print()
-			continue
-		bet = int(input("Your bet?\n"))
-		cho_han(bet, dice)
-		print('-' * 30)
-		print()
+			if again == 'y':
+				continue
+			else:
+				rolling = False
+	elif game == 'h':
+		drawing = True
+		while drawing:
+			try:
+				bet = int(input("Your bet?\n"))
+			except ValueError:
+				print("Bet must be an integer")
+				print()
+				continue
+			if bet > money:
+				print("You don't have enough money to make that bet")
+				print()
+				continue
+			print('-' * 30)
+			highest_card(bet)
+			print('-' * 30)
+			print("You have ${} remaining".format(money))
+			if money == 0:
+				drawing = False
+				continue
+			again = input("Draw again? (y/n)\n").lower()
+			print()
+			if again == 'y':
+				continue
+			else:
+				drawing = False
+	elif game == 'r':
+		spinning = True
+		roulette_bets = {}
+		while spinning:
+			print()
+			type_of_bet = input("""Place your bets on the table:
+[N]umber
+[O]dd
+[E]ven
+[R]ed
+[B]lack
+[S]pin
+""").lower()
+			if type_of_bet == 'n':
+				number_to_bet = input("What number whould you like to bet?\n")
+				if number_to_bet == '00':
+					number_to_bet = '-1'
+				try:
+					number_to_bet = int(number_to_bet)
+				except ValueError:
+					print("Value must be a number between 00 and 36")
+					print()
+					continue
+				if number_to_bet < -1 or number_to_bet > 36:
+					print("Value must be a number between 00 and 36")
+					print()
+				try:
+					bet = int(input("Your bet?\n"))
+				except ValueError:
+					print("Bet must be an integer")
+					print()
+					continue
+				if bet > money:
+					print("You don't have enough money to make that bet")
+					print()
+					continue
+				money -= bet
+				roulette_bets[number_to_bet] = bet
+			elif type_of_bet == 'o':
+				try:
+					bet = int(input("Your bet?\n"))
+				except ValueError:
+					print("Bet must be an integer")
+					print()
+					continue
+				if bet > money:
+					print("You don't have enough money to make that bet")
+					print()
+					continue
+				money -= bet
+				roulette_bets['odd'] = bet
+			elif type_of_bet == 'e':
+				try:
+					bet = int(input("Your bet?\n"))
+				except ValueError:
+					print("Bet must be an integer")
+					print()
+					continue
+				if bet > money:
+					print("You don't have enough money to make that bet")
+					print()
+					continue
+				money -= bet
+				roulette_bets['even'] = bet
+			elif type_of_bet == 'r':
+				try:
+					bet = int(input("Your bet?\n"))
+				except ValueError:
+					print("Bet must be an integer")
+					print()
+					continue
+				if bet > money:
+					print("You don't have enough money to make that bet")
+					print()
+					continue
+				money -= bet
+				roulette_bets['red'] = bet
+			elif type_of_bet == 'b':
+				try:
+					bet = int(input("Your bet?\n"))
+				except ValueError:
+					print("Bet must be an integer")
+					print()
+					continue
+				if bet > money:
+					print("You don't have enough money to make that bet")
+					print()
+					continue
+				money -= bet
+				roulette_bets['black'] = bet
+			elif type_of_bet =='s':
+				print('-' * 30)
+				roulette(roulette_bets)
+				print('-' * 30)
+				print("You have ${} remaining".format(money))
+				if money == 0:
+					spinning = False	
+					continue			
+				again = input("Spin again? (y/n)\n")
+				if again == 'y':
+					roulette_bets = {}
+					continue
+				else:
+					spinning = False	
 	elif game == 'q':
 		print("You walked away with ${}".format(money))
 		if money > 1000:
 			print("Great job!")
 		else:
 			print("Better luck next time.")
-		break
-# coin_flip(10, "tails")
-# cho_han(20, "even")
-# highest_card(50)
-# roulette_bets = {'red': 10, 'even': 10, 'odd': 50, 22: 10}
-# print(roulette(roulette_bets))
+		playing = False
